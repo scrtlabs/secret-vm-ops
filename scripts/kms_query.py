2801f4f87f64ec7e95ddde4d13fee9ea982fe51f
vlad <vlad@scrtlabs.com>
2025-02-24 10:28:32 +0000

    added kms_query.py

---
+++ scripts/kms_query.py
@@ -0,0 +1,33 @@
+import sys
+import json
+from secret_sdk.client.lcd import LCDClient
+
+if len(sys.argv) > 3:
+    svc_id = int(sys.argv[1])
+    quote = list(bytearray.fromhex(sys.argv[2]))
+    collateral = list(bytearray.fromhex(sys.argv[3]))
+
+    client = LCDClient(chain_id="secretdev-1", url="http://51.8.118.178:1317")
+
+    # Construct the query message.
+    query_msg = {
+        "get_secret_key": {
+            "service_id": svc_id,
+            "quote": quote,
+            "collateral": collateral,
+        }
+    }
+
+    # Query the contract.
+    result = client.wasm.contract_query("secret17p5c96gksfwqtjnygrs0lghjw6n9gn6c804fdu", query_msg)
+
+    # The response should look like this:
+    # {
+    #   "encrypted_secret_key": "xxxxx",
+    #   "export_key": "yyyyy"
+    # }
+
+    print(result.get('encrypted_secret_key'))
+
+else:
+    print(f"Usage: service_id hex_encoded(quote) hex_encoded(collateral)")
