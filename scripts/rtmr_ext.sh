5be8f1d11db2329dd4e99af66990050ed58e523e
vlad <vlad@scrtlabs.com>
2025-03-09 18:35:10 +0000

    rtmr_ext more flexible

--- scripts/rtmr_ext.sh
+++ scripts/rtmr_ext.sh
@@ -4,22 +4,29 @@ test_valid_hex384()
 {
     local var_name="$1"
     local var_value="${!var_name}"
+	var_value=$(printf "%-96s" "$var_value" | tr ' ' '0')
 
-    if [[ -n "$var_value" && "$var_value" =~ ^[[:xdigit:]]+$ ]]; then
-	    if [ ${#var_value} -eq 96 ]; then
-        	return 0
-		fi
-        echo "Incorrect length (must be 48 bytes) " $var_name ": " $var_value
+    if [[ "$var_value" =~ ^[[:xdigit:]]+$ ]]; then
+		eval "$var_name=\"\$var_value\"" # assign adjusted value back
     else
         echo "Not a valid hex string " $var_name ": " $var_value
+		exit 1
     fi
-
-	exit 1
 }
 
+if [ ! -n "$1" ]; then
+	echo "Usage rtmr_ext [rtmr_prev] new_mesaurement"
+	exit 1
+fi
 
-rtmr_prev=$1
-new_val=$2
+
+if [ ! -n "$2" ]; then
+	rtmr_prev=""
+	new_val=$1
+else
+	rtmr_prev=$1
+	new_val=$2
+fi
 
 test_valid_hex384 "rtmr_prev"
 test_valid_hex384 "new_val"
